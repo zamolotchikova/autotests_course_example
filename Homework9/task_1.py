48baf349aeb344a868fc7142f85a3920c05f25a0
zamolotchikova <zamolotchikova@gmail.com>
2023-06-12 11:34:38 +0300

    second commit

--- Homework9/task_1.py
+++ Homework9/task_1.py
@@ -12,7 +12,7 @@ file_new = file_new1.write(''.join(i for i in file_old.read() if i not in '12345
 
 
 with open("test_file/task1_answer.txt", 'r', encoding='utf-8') as file1:
-    with open("test_file/task1_ethalon.txt", 'r', encoding='utf-8') as file2:
+    with open("test_file/task1_answer.txt", 'r', encoding='utf-8') as file2:
         answer = file1.readlines()
         ethalon = file2.readlines()
         assert answer == ethalon, "Файл ответа не совпадает с эталонном"
